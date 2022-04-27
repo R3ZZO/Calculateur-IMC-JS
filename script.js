fe8be0d50b2c891ed76c57d07eba7fa7d55ca843
Quentin B <93120048+R3ZZO@users.noreply.github.com>
2022-04-27 13:35:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("valider").onclick = calcul;
+
+
+var resultat = document.getElementById("resultat");
+
+
+function calcul(){
+    var poids = document.getElementById("poids").value;
+    var taille = document.getElementById("taille").value;
+    var imc = (poids*10000) / (taille*taille);
+    if (imc <= 1){
+        document.getElementById("resultat").innerHTML = "<span class='erreur'>Merci d'indiquer un poids et une taille correcte</span>";
+    }
+    else if (imc <= 18 ){
+        document.getElementById("resultat").innerHTML = "<span class='maigre'>Vous êtes anoréxique" + "<br>Votre imc est de: " + imc.toFixed(2) + "</span>";
+        
+    } else if (imc <= 25){
+        document.getElementById("resultat").innerHTML = "<span class='parfait'>Votre poids est parfait" + "<br>Votre imc est de: " + imc.toFixed(2)+ "</span>";
+    } else if (imc <= 30){
+        document.getElementById("resultat").innerHTML = "<span class='surpoids'>Vous êtes en surpoids" + "<br>Votre imc est de: " + imc.toFixed(2)+ "</span>";
+    } else if (imc <= 35){
+        document.getElementById("resultat").innerHTML = "<span class='obese'>Vous êtes un gros lard" + "<br>Votre imc est de: " + imc.toFixed(2)+ "</span>";
+    }else if (imc > 35){
+            document.getElementById("resultat").innerHTML = "<span class='mort'>Vous êtes bientôt mort" + "<br>Votre imc est de: " + imc.toFixed(2)+ "</span>";
+    }
+
+    console.log(imc);
+}
